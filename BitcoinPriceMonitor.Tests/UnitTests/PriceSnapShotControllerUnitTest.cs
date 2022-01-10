1225d263c13273a8bc97634397d7827804ad3677
gadamou <gadamou100@hotmail.com>
2022-01-10 15:38:46 +0200

    Add unit test for edit and delete  for the price snapshot controller.

--- BitcoinPriceMonitor.Tests/UnitTests/PriceSnapShotControllerUnitTest.cs
+++ BitcoinPriceMonitor.Tests/UnitTests/PriceSnapShotControllerUnitTest.cs
@@ -3,7 +3,10 @@ using BitcoinPriceMonitor.Application.Interfaces;
 using BitcoinPriceMonitor.Controllers;
 using BitcoinPriceMonitor.ViewModels;
 using BitCoinPriceMonitor.Domain.Data.Entities;
+using CSharpFunctionalExtensions;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -35,39 +38,102 @@ namespace BitcoinPriceMonitor.Tests.UnitTests
             Assert.Equal("00000000-0000-0000-0000-000000000001", model?.ListItems?.Items?.FirstOrDefault()?.Id);
         }
 
-        private static List<PriceSnapshot> GetData()
+        [Fact]
+        public async Task TestPriceSnapShotGetModelForEdit()
         {
-            return new List<PriceSnapshot>()
-            {
-                 new PriceSnapshot
-                {
+            var pageListMock = MockPagedListGetter.GetPageListMock();
+            var serviceMock = new Mock<IPriceSnapshotService>();
+            serviceMock.Setup(x => x.FindById(It.IsAny<string>())).Returns(Task.FromResult(Maybe.From(new PriceSnapshot { Id="test",Value=1})));
+            var controller = new PriceSnapShotController(serviceMock.Object);
+            //Act
+            var actionResult = await controller.Edit("test");
+            //Asser
+            var viewResult = actionResult as ViewResult;
+            var model = viewResult?.Model as PriceSnapshot;
+            Assert.Equal("test", model?.Id);
+            Assert.Equal(1, model?.Value);
+        }
 
-                Id = "00000000-0000-0000-0000-000000000001",
-                CreatedTimeStamp = DateTime.MaxValue,
-                PriceSourceId ="Mock"
-                },
-                  new PriceSnapshot
-                {
+        [Fact]
+        public async Task TestPriceSnapShotGetModelForEditNotFound()
+        {
+            var pageListMock = MockPagedListGetter.GetPageListMock();
+            var serviceMock = new Mock<IPriceSnapshotService>();
+            serviceMock.Setup(x => x.FindById(It.IsAny<string>())).Returns(Task.FromResult(Maybe<PriceSnapshot>.None));
+            var controller = new PriceSnapShotController(serviceMock.Object);
+            //Act
+            var actionResult = await controller.Edit("test");
+            //Asser
+            var notFoundResult = actionResult as NotFoundResult;
+            Assert.Equal(404, notFoundResult.StatusCode);
+        }
 
-                Id = "00000000-0000-0000-0000-000000000002",
-                CreatedTimeStamp = DateTime.MaxValue,
-                PriceSourceId ="Mock"
-                },
-                    new PriceSnapshot
-                {
 
-                Id = "00000000-0000-0000-0000-000000000003",
-                CreatedTimeStamp = DateTime.MaxValue,
-                PriceSourceId ="Mock"
-                },
-                  new PriceSnapshot
-                {
+        [Fact]
+        public async Task TestPriceSnapShotEdit()
+        {
+            var pageListMock = MockPagedListGetter.GetPageListMock();
+            var serviceMock = new Mock<IPriceSnapshotService>();
+            serviceMock.Setup(x => x.Edit(It.IsAny<PriceSnapshot>(),It.IsAny<string>()));
+            var formCollectionMock = new Mock<IFormCollection>();
+            StringValues comments = "Comments";
+            formCollectionMock.Setup(p => p.TryGetValue(nameof(PriceSnapshot.Comments), out comments)).Returns(true);
+            var controller = new PriceSnapShotController(serviceMock.Object);
+            //Act
+            var actionResult = await controller.Edit("test",formCollectionMock.Object);
+            //Asser
+            var redirectResult = actionResult as RedirectToActionResult;
+            Assert.Equal("Index", redirectResult.ActionName);
+        }
 
-                Id = "00000000-0000-0000-0000-000000000004",
-                CreatedTimeStamp = DateTime.MaxValue,
-                PriceSourceId ="Mock"
-                },
-            };
+
+
+        [Fact]
+        public async Task TestPriceSnapShotGetModelForDelete()
+        {
+            var pageListMock = MockPagedListGetter.GetPageListMock();
+            var serviceMock = new Mock<IPriceSnapshotService>();
+            serviceMock.Setup(x => x.FindById(It.IsAny<string>())).Returns(Task.FromResult(Maybe.From(new PriceSnapshot { Id = "test", Value = 1 })));
+            var controller = new PriceSnapShotController(serviceMock.Object);
+            //Act
+            var actionResult = await controller.Delete("test");
+            //Asser
+            var viewResult = actionResult as ViewResult;
+            var model = viewResult?.Model as PriceSnapshot;
+            Assert.Equal("test", model?.Id);
+            Assert.Equal(1, model?.Value);
+        }
+
+        [Fact]
+        public async Task TestPriceSnapShotGetModelForDeleterNotFound()
+        {
+            var pageListMock = MockPagedListGetter.GetPageListMock();
+            var serviceMock = new Mock<IPriceSnapshotService>();
+            serviceMock.Setup(x => x.FindById(It.IsAny<string>())).Returns(Task.FromResult(Maybe<PriceSnapshot>.None));
+            var controller = new PriceSnapShotController(serviceMock.Object);
+            //Act
+            var actionResult = await controller.Delete("test");
+            //Asser
+            var notFoundResult = actionResult as NotFoundResult;
+            Assert.Equal(404, notFoundResult.StatusCode);
+        }
+
+
+
+        [Fact]
+        public async Task TestPriceSnapShotDelete()
+        {
+            var pageListMock = MockPagedListGetter.GetPageListMock();
+            var serviceMock = new Mock<IPriceSnapshotService>();
+            serviceMock.Setup(x => x.Delete(It.IsAny<string>()));
+            var formCollectionMock = new Mock<IFormCollection>();
+            StringValues comments = "Comments";
+            var controller = new PriceSnapShotController(serviceMock.Object);
+            //Act
+            var actionResult = await controller.Delete("test", formCollectionMock.Object);
+            //Asser
+            var redirectResult = actionResult as RedirectToActionResult;
+            Assert.Equal("Index", redirectResult.ActionName);
         }
     }
 }
